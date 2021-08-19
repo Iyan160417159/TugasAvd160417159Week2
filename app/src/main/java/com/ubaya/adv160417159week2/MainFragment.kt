a421341cfd3c5ba3cbc119b5d4c1ef66e8922e64
IyanA <s160417159@student.ubaya.ac.id>
2021-08-19 16:14:58 +0700

    Tugas 1

--- app/src/main/java/com/ubaya/adv160417159week2/MainFragment.kt
+++ app/src/main/java/com/ubaya/adv160417159week2/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment() {
@@ -17,5 +19,14 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnStart.setOnClickListener{
+            val playerName= txtName.text.toString()
+            val action= MainFragmentDirections.actionGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
